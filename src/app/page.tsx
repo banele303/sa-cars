fe3d570a73d7917d3f3c3772dd6a18f285ebc976
alex banele <alexsouthflow@gmail.com>
2025-02-14 21:52:45 +0200

    basic clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import Themetoggle from "@/components/shared/theme-toggle";
+import { UserButton } from "@clerk/nextjs";
+
 
 export default function Home() {
   return (
-    <div className="bg-blue-800">
-     <h1>Hello there if uts working</h1>
+    <div className="p-5">
+      <div className="w-100 flex gap-4 justify-end">
+        <UserButton/>
+<Themetoggle/>
+      </div>
+     <h1 className="font-geist text-2xl">Hello there if uts working</h1>
+     <h1 className="font-barlow text-2xl">Hello there if uts working</h1>
     </div>
   );
 }
